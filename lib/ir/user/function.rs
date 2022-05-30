6ebd1906fa4f85137971c8dd1470f837ddb8668b
SjVer <sjoerd@marsenaar.com>
2022-05-30 20:15:42 +0200

    upload

--- lib/ir/user/function.rs
+++ lib/ir/user/function.rs
@@ -4,29 +4,3 @@
 // For more info see https://github.com/SjVer/EvIr
 //===--------------------------------------------===
 
-use super::{
-	UserT,
-	super::IR,
-};
-
-pub struct Function {
-	// blocks: Vec<BasicBlock>,
-}
-
-// user stuff
-impl UserT for Function {
-	fn is_defined(&self) -> bool {
-		// !self.blocks.is_empty()
-		false
-	}
-
-	fn generate_ir(&self) -> IR {
-		// properties
-		let mut ir = self.generate_properties_comment();
-
-		// signature
-		// ir += &format!("{} {}\n", self.get_name(), self.)
-
-		ir
-	}
-}
--- lib/ir/user/mod.rs
+++ lib/ir/user/mod.rs
@@ -10,42 +10,13 @@ pub use function::*;
 
 use super::{IR, generate_ir_comment, Module};
 
-pub struct User<'a> {
-	name: Option<String>,
-	properties: Vec<String>,
-	user: UserE,
-	parent: &'a mut Module<'a>,
+/*
+fn generate_properties_comment(&self) -> IR {
+	let mut props = self.properties.clone();
+	props.push(String::from(if self.user.is_defined() { "defined" } else { "declared" }));
+	
+	if !props.is_empty() {
+		generate_ir_comment(format!("props: {}", props.join(", ")), false)
+	} else { IR::new() }
 }
-
-impl<'a> User<'a> {
-	fn get_name(&self) -> Option<String> {
-		self.name.clone()
-	}
-
-	fn get_parent(&'a self) -> &'a mut Module {
-		self.parent
-	}
-
-	fn add_property(&mut self, property: impl ToString) {
-		self.properties.push(property.to_string());
-	}
-
-	fn generate_properties_comment(&self) -> IR {
-		let mut props = self.properties.clone();
-		props.push(String::from(if self.user.is_defined() { "defined" } else { "declared" }));
-		
-		if !props.is_empty() {
-			generate_ir_comment(format!("props: {}", props.join(", ")), false)
-		} else { IR::new() }
-	}
-}
-
-enum UserE {
-	Function(Function),
-	// Global(Global),
-}
-
-pub trait UserT {
-	fn is_defined(&self) -> bool;
-	fn generate_ir(&self) -> IR;
-}
+*/
